d3f542816587e426eb02b0aa96b60ea2fb71f182
yashwanth <yashwanth24710@gmail.com>
2025-04-05 13:22:08 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -2,8 +2,17 @@ const express = require("express");
 const app = express();
 
 app.get("/test",(req,res) => {
-    res.send("hello from the server")
+    res.send("hello from the server");
 });
+
+app.get("/user/:userid/:name/:password",(req,res)=>{
+    console.log(req.params);
+    res.send({Name : "yashwanth", profession: "full stack developer"});
+});
+app.post(/.*fly$/,(req,res)=>{
+    console.log(req.query);
+    res.send({Name : "yashwanth", profession: "full stack developer",lastName: "kancharla"});
+})
 
 app.get("/",(req,res) => {
     res.send("hi from the server")
